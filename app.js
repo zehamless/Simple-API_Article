65f626edca79e875208e38dc0f5dbff3d5a5e2ba
petersonhz25 <82883040+zehamless@users.noreply.github.com>
2023-05-11 16:34:03 +0700

    ADD GET REQUEST

--- app.js
+++ app.js
@@ -1,55 +1,91 @@
-const bodyParser = require('body-parser');
-const express = require('express');
-const  ejs = require('ejs');
-const { loweCase } = require('lodash');
-const { MongoClient } = require('mongodb');
-const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
+const express = require("express");
+const ejs = require("ejs");
+const { loweCase } = require("lodash");
+const { MongoClient } = require("mongodb");
+const mongoose = require("mongoose");
 const app = express();
 
-app.set('view engine','ejs');
+app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 res = app.get;
 
-const uri = 'mongodb://localhost:27017/wiki';
+const uri = "mongodb://localhost:27017/wiki";
 mongoose.connect(uri);
 
 const articleSchema = {
-    title: String,
-    text: String,
+  title: String,
+  text: String,
 };
 
-const Articles = mongoose.model('wiki', articleSchema);
+const Articles = mongoose.model("wiki", articleSchema);
 
-app.get("/article", function(req, res) {
+app
+  .route("/article")
+  .get(function (req, res) {
     Articles.findOne({})
-    .then(function(found) {
+      .then(function (found) {
         console.log(found);
         res.send(found);
-    })
-    .catch(function(err) {
+      })
+      .catch(function (err) {
         console.log(err);
         handleError(err);
-    });
-});
-
-app.post('/article', function(req, res) {
+      });
+  })
+  .post(function (req, res) {
     const post = {
-        title: req.body.title,
-        text: req.body.text,
-    }
-    newPost = new Articles(post)
-    newPost.save().then(function(result){
-        console.log("Added"+ result);
+      title: req.body.title,
+      text: req.body.text,
+    };
+    newPost = new Articles(post);
+    newPost
+      .save()
+      .then(function (result) {
+        console.log("Added" + result);
         res.send("Success");
-    })
-    .catch(function(err){
+      })
+      .catch(function (err) {
         console.log(err);
         res.send(err.message);
-    });
-});
+      });
+  })
+  .delete(function (req, res) {
+    Articles.deleteMany()
+      .then(function (item) {
+        res.send("Delete article");
+      })
+      .catch(function (err) {
+        res.send(err.message);
+      });
+  });
 
-app.listen(3000,function(){
-    console.log("Service listening");
+app.route('/article/:articleId')
+  .get(function(req, res){
+    const articleId = req.params.articleId;
+    Articles.findById(articleId)
+      .then(function (item) {
+        console.log(item);
+        res.send(item);
+      })
+      .catch(function (err) {
+        console.log(err.message);
+        res.send("Article not found");
+      });
+  })
+  .put(function(req, res){
+    const articleId = req.params.articleId;
+    const updated = {
+      title: req.body.title;
+      text: req.body.text;
+    }
+    Articles.findByIdAndUpdate()
+  })
+
+
+app.delete("/article");
+app.listen(3000, function () {
+  console.log("Service listening");
 });
